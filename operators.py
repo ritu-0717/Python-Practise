8acf30aa0530e4afbb8c74852fe7a0a7fa0ac066
ritu-0717 <rituranipradhan2707@gmail.com>
2026-08-20 14:39:35 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,95 @@
+# #airthmetic operator
+# # from operator import true
+#
+# # x=10
+# # y=20
+# #
+# # print(x+y)
+# # print(x-y)
+# # print(x*y)
+# # print(x/y)#division
+# # print(x//y)#floor division remove decimal value
+# # print(x%y)# modulus -remainder
+# # print(x**y) #to count power
+# #---------------------------------------------------------------------------------------------------------------------
+# # #comparison operator
+# # x=10
+# # y=5
+# # print(x==y)
+# # print(x!=y)
+# # print(x>y)
+# # print(x<y)
+# # print(x>=y)
+# # print(x<=y)
+# #---------------------------------------------------------------------------------------------------------------------
+# #logical operator
+# '''
+# and- all condition must be true
+# or- at least one condition must be true
+# not- single operand,reverse
+# '''
+# age=20
+# is_student=True
+#
+# print(age>18 and is_student)
+# print(age<25 or is_student)
+# print (not is_student)
+#
+# a=10
+# b=20
+# print(a>5 and b>10)
+# print(a<1 or b==20)
+# print(not(a==10))
+#----------------------------------------------------------------------------------------------------------------------
+#assignment operator
+# =
+# +=
+# -+
+# *=
+# x=10
+# print(x)
+# x+=5
+# print(x)
+# x-=5
+# print(x)
+# x*=5
+# print(x)
+
+
+#----------------------------------------------------------------------------------------------------------------------
+# identity operator
+# is - True location same true
+# is not - True not same location true
+#
+# a=[1,2,3]
+# b=a
+# c=[1,2,3]
+#
+# print(a is b)
+# print(a is c)
+
+# in this it doesnt see same dta it only check location
+
+# ----------------------------------------------------------------------------------------------------------------------
+# membership operator
+# in-True
+# not in - True
+# in - True
+# not in - TRUE
+
+# vegetables =['tomato','potato','brinjal']
+# print("potato" in vegetables)
+# print("bhindi" in vegetables)
+# in int like 1234 value it doent give output
+# -----------------------------------------------------------------------------------------------------------------------
+
+# summary-----
+
+# airthmatic = +,-,*,/,//,%.**
+# logical = true or false and ,or,not
+# comparison = true or false ==,=>,=<, <,>
+# membership = is ,is not memory location
+# identity = in,not in
+# assignment = x=+,x=-,x=*
+a = 10
+b = 20
