a4657d420791a11152bb83786e36c5b8196e21ea
ritu-0717 <rituranipradhan2707@gmail.com>
2026-08-21 14:00:14 +0530

    Add files via upload

---
+++ BASICS.py
@@ -0,0 +1,49 @@
+
+# 1. Check whether a number is positive, negative, or zero
+a = 8
+
+if a > 0:
+    print("number is positive")
+elif a < 0:
+    print("number is negative")
+else:
+    print("number is zero")
+
+
+# 2. Check whether a number is even or odd
+a = 4
+
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+
+# 3. Check whether a person is eligible to vote
+age = 18
+
+if age >= 18:
+    print("you can vote")
+else:
+    print("you cannot vote")
+
+
+# 4. Check whether a number is divisible by 5
+a = 25
+
+if a % 5 == 0:
+    print("divisible by 5")
+else:
+    print("not divisible by 5")
+
+
+# 5. Find the greater of two numbers
+a = 255
+b = 29
+
+if a > b:
+    print("a is greater than b")
+elif b > a:
+    print("b is greater than a")
+else:
+    print("both are equal")
